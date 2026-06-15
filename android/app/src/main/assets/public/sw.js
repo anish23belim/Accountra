c5a95b6afed4019754c3aeb1c548872d68cab24c
anish23belim <anish23belim@gmail.com>
2026-06-16 00:20:03 +0530

    Commit generated capacitor files and remove sync

---
+++ android/app/src/main/assets/public/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Let the browser do its default thing
+  // A fetch listener is required to pass the PWA installability criteria
+});
